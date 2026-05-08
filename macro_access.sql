33fd114ee75b60e207ca98758e0003f148942505
Janice Recto <133826665+mugiwaranru@users.noreply.github.com>
2026-05-09 00:28:49 +0800

    Add files via upload

---
+++ macro_access.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2026 at 06:27 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `macro_access`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test_records`
+--
+
+CREATE TABLE `test_records` (
+  `id` int(11) NOT NULL,
+  `client_name` varchar(100) NOT NULL,
+  `age` int(3) DEFAULT NULL,
+  `sex` char(1) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `company_name` varchar(150) DEFAULT NULL,
+  `meth_result` enum('NEGATIVE','POSITIVE','INVALID') DEFAULT 'NEGATIVE',
+  `thc_result` enum('NEGATIVE','POSITIVE','INVALID') DEFAULT 'NEGATIVE',
+  `photo_path` varchar(255) DEFAULT NULL,
+  `date_tested` datetime DEFAULT current_timestamp(),
+  `added_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `position` enum('ADMIN','CHIEF TECHNOLOGIST','STAFF') DEFAULT 'STAFF',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `position`, `created_at`) VALUES
+(1, 'admin', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'CHIEF TECHNOLOGIST', '2026-05-08 16:08:55');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `test_records`
+--
+ALTER TABLE `test_records`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `added_by` (`added_by`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `test_records`
+--
+ALTER TABLE `test_records`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `test_records`
+--
+ALTER TABLE `test_records`
+  ADD CONSTRAINT `test_records_ibfk_1` FOREIGN KEY (`added_by`) REFERENCES `users` (`id`) ON DELETE SET NULL;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
